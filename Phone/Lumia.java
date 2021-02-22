fbccb265741dbb2662ec59f8a0a09a866dc31496
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-22 19:26:20 +0200

    Update Lumia.java

--- Phone/Lumia.java
+++ Phone/Lumia.java
@@ -2,6 +2,11 @@ package com.app;
 
 public class Lumia extends MobilePhone{
     protected float m_ms_version;
+    
+    public Lumia(String m_color, float m_size, float m_ms_version) {
+    super(m_color, m_size);
+    this.m_ms_version = m_ms_version;
+    }
 
     public float getMsOs(){
         return m_ms_version;
