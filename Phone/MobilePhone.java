f37f5861623475442ba6904c7fb56434acced302
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-22 19:25:02 +0200

    Update MobilePhone.java

--- Phone/MobilePhone.java
+++ Phone/MobilePhone.java
@@ -3,6 +3,11 @@ package com.app;
 public class MobilePhone {
     protected String m_color;
     protected float m_size;
+    
+    public MobilePhone(String m_color, float m_size) {
+    this.m_color = m_color;
+    this.m_size = m_size;
+}
 
     @Override
     public String toString() {
