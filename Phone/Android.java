13ebc84cadd7b6af41675c9ad29c6955165fb80f
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-22 19:25:31 +0200

    Update Android.java

--- Phone/Android.java
+++ Phone/Android.java
@@ -3,6 +3,11 @@ package com.app;
 public class Android extends MobilePhone{
     protected float m_andoird_os;
 
+    public Android(String m_color, float m_size, float m_andoird_os) {
+    super(m_color, m_size);
+    this.m_andoird_os = m_andoird_os;
+    }
+    
     public float getAndroidOs (){
         return m_andoird_os;
     }
