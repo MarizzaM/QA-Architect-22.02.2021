84ed7b367350a00224aa31afde4f1ab3893147df
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-02-22 19:25:56 +0200

    Update Iphone.java

--- Phone/Iphone.java
+++ Phone/Iphone.java
@@ -6,6 +6,11 @@ public class Iphone extends MobilePhone{
     public float getIphoneOs(){
         return m_ios_version;
     }
+    
+    public Iphone(String m_color, float m_size, float m_ios_version) {
+    super(m_color, m_size);
+    this.m_ios_version = m_ios_version;
+    }
 
     @Override
     public String toString() {
